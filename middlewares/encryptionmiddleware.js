f1bfe017bf201eb1c9b90fa73ebd2840bf3d34b4
AnoopSathyaseelan <anoopsathyaseelan@gmail.com>
2022-12-19 19:36:11 +0530

    NEW: Encyrt middleware added

--- middlewares/encryptionmiddleware.js
+++ middlewares/encryptionmiddleware.js
@@ -1,4 +1,23 @@
 
-exports.encryption=(data)=>{
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc'; //Using AES encryption
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 
+//Encrypting text
+exports.encrypt = async(text) => {
+   let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+   let encrypted = cipher.update(text);
+   encrypted = Buffer.concat([encrypted, cipher.final()]);
+   return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+
+// Decrypting text
+exports.decrypt=(text)=> {
+   let iv = Buffer.from(text.iv, 'hex');
+   let encryptedText = Buffer.from(text.encryptedData, 'hex');
+   let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+   let decrypted = decipher.update(encryptedText);
+   decrypted = Buffer.concat([decrypted, decipher.final()]);
+   return decrypted.toString();
 }
